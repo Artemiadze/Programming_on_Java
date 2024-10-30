b028f5ff7f3013ee92173201a95efd7a466fcc73
Artemiadze <t0mi.v@yandex.ru>
2024-10-30 10:48:58 +0300

    add second version second homework

--- Homeworks/Frequency_DIctionary/src/Dictionary.java
+++ Homeworks/Frequency_DIctionary/src/Dictionary.java
@@ -1,9 +1,40 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
+//класс для работы с файлами
 public class Dictionary {
+    public void countAndWriteCharacterUsage(String fileName) {
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
 
+            // чтение посимвольно
+            int[] charCount = new int[26]; // массив для подсчета использования english символов
+            int symbol;
+
+            //проверка на то, что данная буква из английского алфавита
+            while (( symbol= fileReader.read()) != -1) {
+                if (Character.isLetter(symbol) && Character.toLowerCase(symbol) >= 'a' &&
+                        Character.toLowerCase(symbol) <= 'z') {
+                    charCount[Character.toLowerCase(symbol) - 'a']++;
+                }
+            }
+            //отступ в файле для удобства
+            writer.write( "\n");
+
+            //запись результата в файл
+            for (int i = 0; i < 26; i++) {
+                writer.write(((char)('a' + i)) + ": " + charCount[i] + "\n");
+            }
+
+            fileReader.close();
+            writer.close();
+
+            System.out.println("The result is successfully written to the specified file.");
+
+        } catch (FileNotFoundException ExObj1) {
+            System.out.println("File '"+fileName+"' not found or not exist!");
+        } catch (IOException ExObj2) {
+            System.out.println("I/O error!");
+        }
+    }
 }
--- Homeworks/Frequency_DIctionary/src/Main.java
+++ Homeworks/Frequency_DIctionary/src/Main.java
@@ -3,34 +3,18 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName;
+        String file_name = "";
 
         try {
             System.out.println("Введите имя файла:");
-            fileName = reader.readLine();
+            file_name = reader.readLine();
 
-            FileReader fileReader = new FileReader(fileName);
-            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+            //начало работы с классом
+            Dictionary work_with_my_file = new Dictionary();
+            work_with_my_file.countAndWriteCharacterUsage(file_name);
 
-            int[] charCount = new int[26]; // массив для подсчета использования символов
-            int c;
-
-            while ((c = fileReader.read()) != -1) {
-                if (Character.isLetter(c)) {
-                    charCount[Character.toLowerCase(c) - 'a']++;
-                }
-            }
-            writer.write( "\n"); //перевод строки
-            for (int i = 0; i < 26; i++) {
-                writer.write(((char)('a' + i)) + ": " + charCount[i] + "\n");
-            }
-
-            fileReader.close();
-            writer.close();
-
-            System.out.println("Результаты успешно записаны в файл.");
-        } catch (IOException e) {
-            System.out.println("Ошибка ввода названия файла: " + e.getMessage());
+        } catch (IOException ExObj1) {
+            System.out.println("I/O error:" + ExObj1.getMessage());
         }
     }
 }
